d76050a637d43bad99d65bfa9d2823542b2f4f4a
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2022-11-26 13:28:12 +0530

    modify seo component

--- src/components/seo.component.js
+++ src/components/seo.component.js
@@ -4,27 +4,23 @@ import { Helmet } from "react-helmet-async";
 const SEO = ({ title, description, url, image }) => {
   return (
     <Helmet defer={false}>
+      {/* Primary Meta Tags */}
       <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+      <link rel="icon" href="/favicon.ico" />
 
-      <meta name="description" content={description}/>
-      <meta name="author" content="Miluckshan J. Vidyajayan" />
-      <meta
-        name="keywords"
-        content="React Helmet, React, Miluckshan, Miluckshan J. Vidyajayan"
-      />
-
-      <meta property="og:title" content={title} />
-      <meta property="og:url" content={url} />
-      <meta property="og:image" content={image} />
+      {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:locale" content="en_GB" />
+      <meta property="og:image" content={image} />
 
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:image" content={image} />
-      <meta name="twitter:card" content="website" />
-      <meta name="twitter:creator" content="miluckshan_j" />
-      <meta name="twitter:description" content={description} />
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
     </Helmet>
   );
 };
